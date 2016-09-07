315f3b944d5d025d76e534559b2374625b40942a
Omisakin Oluwatobi Samuel <oluwatob@ut.ee>
2016-09-07 15:47:53 +0300

    increase the size of the Add music dialog

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,7 +46,7 @@
 
         <!-- Modal -->
         <div class="modal fade" id="myModal" role="dialog">
-            <div class="modal-dialog modal-lg">
+            <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
