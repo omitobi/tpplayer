f017a0536698301ac1863c45f8dc37fb3bcd2977
Omisakin Oluwatobi Samuel <oluwatob@ut.ee>
2016-12-10 16:42:49 +0200

    Initialize replay

--- public/js/musics.js
+++ public/js/musics.js
@@ -1,12 +1,18 @@
 $(document).ready( function() {
     var player = document.getElementById('player');
-    
+
     $(player).attr('src', 'http://lgfkc.gcichurches.org/worshipmusic/mp3/Glorious Day (Living He Loved Me).mp3');
 
-    playNow();
+    playNow(player);
 
-    function playNow() {
-        player.play();
+    function playNow($playerVar) {
+        $playerVar.play();
     }
+
+    //When the track ends, play again
+    player.on('ended', function() {
+        $(player).attr('src', 'http://lgfkc.gcichurches.org/worshipmusic/mp3/Glorious Day (Living He Loved Me).mp3');
+        playNow(player);
+    });
 
 });
