bfa2e90dd4de1f2b87a362b6991ba8caa22ec7c2
Omisakin Oluwatobi Samuel <oluwatob@ut.ee>
2016-12-10 15:31:14 +0200

    Use included music list

--- resources/views/musics/index.blade.php
+++ resources/views/musics/index.blade.php
@@ -10,26 +10,26 @@
                     <div class="panel-body">
                         @include('includes.set')
                         @include('includes.tool')
-                        {{--@include('includes.list')--}}
-                        <div id="list" class="pull-right" style="width: 60%">
-                            <table class="table table-striped">
-                                <thead>
-                                <tr>
-                                    <th>Name </th>
-                                    <th>Link</th>
-                                    <th style="width: 10px">Available</th>
-                                </tr>
-                                </thead>
-                                <tbody id="fullList">
-                                @foreach($musics as $music)
-                                    <tr>
-                                        <td>{{$music->name}}</td>
-                                        <td>{{$music->link}}</td>
-                                        <td>{{$music->duration}}</td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
+                        @include('includes.list')
+                        {{--<div id="list" class="pull-right" style="width: 60%">--}}
+                            {{--<table class="table table-striped">--}}
+                                {{--<thead>--}}
+                                {{--<tr>--}}
+                                    {{--<th>Name </th>--}}
+                                    {{--<th>Link</th>--}}
+                                    {{--<th style="width: 10px">Available</th>--}}
+                                {{--</tr>--}}
+                                {{--</thead>--}}
+                                {{--<tbody id="fullList">--}}
+                                {{--@foreach($musics as $music)--}}
+                                    {{--<tr>--}}
+                                        {{--<td>{{$music->name}}</td>--}}
+                                        {{--<td>{{$music->link}}</td>--}}
+                                        {{--<td>{{$music->duration}}</td>--}}
+                                    {{--</tr>--}}
+                                {{--@endforeach--}}
+                                {{--</tbody>--}}
+                            {{--</table>--}}
                         </div>
                     </div>
                 </div>
