9383b358c33de4da1521821b178a68021d7de350
Omisakin Oluwatobi Samuel <oluwatob@ut.ee>
2016-12-12 04:17:01 +0200

    A little disclaimer

--- resources/views/musics/index.blade.php
+++ resources/views/musics/index.blade.php
@@ -31,6 +31,14 @@
                                 {{--</tbody>--}}
                             {{--</table>--}}
                         </div>
+                    <hr>
+                        <div class="panel-body">
+                            <div>
+                                <div><b class="text-danger">Disclaimer!</b></div>
+                                <i>The link you shared is solely your responsibility, and TP_Player developer will not be responsible for it (especially in a case of infringement of intellectual property). You can read more
+                                    <a href="#">Here</a></i>. We do not save the music, and so unless requested any public links would be removed every 30 days.
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
