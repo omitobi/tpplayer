fb6389160c479289d997e693f52a91cbe07c927b
Omisakin Oluwatobi Samuel <oluwatob@ut.ee>
2016-12-12 03:56:37 +0200

    Beautify the control buttons

--- resources/views/includes/tool.blade.php
+++ resources/views/includes/tool.blade.php
@@ -8,14 +8,14 @@
         </audio>
         <div id="controller">
             {{--<a id="dur">Last Duration</a>--}}
-            <a class="button gradient" id="next">Next</a>
-            <a class="button gradient" id="prev">Previous</a>
-            <a class="button gradient" id="pause">Pause</a>
-            <a class="button gradient" id="play">Play</a>
-            <a class="button" id="vup" onclick="document.getElementById('player').volume += 0.1">Up</a>
-            <a class="button" id="vdown" onclick="document.getElementById('player').volume -= 0.1">Dwn</a>
-            <a class="button" id="mute" onclick="document.getElementById('player').volume = 0">Mute </a>
-            <a class="button loadAll" style="cursor: auto">All</a>
+            <a class="btn active small" id="prev"> &lt;&lt;</a>
+            <a class="btn active small" id="next">&gt;&gt;</a>
+            {{--<a class="button gradient" id="pause">Pause</a>--}}
+            {{--<a class="button gradient" id="play">Play</a>--}}
+            <a class="btn btn-primary" id="vup" onclick="document.getElementById('player').volume += 0.1">^</a>
+            <a class="btn btn-success" id="vdown" onclick="document.getElementById('player').volume -= 0.1">v</a>
+            <a class="btn btn-danger" id="mute" onclick="document.getElementById('player').volume = 0">(X)</a>
+            {{--<a class="button loadAll" style="cursor: auto">All</a>--}}
         </div>
     </div>
 </div>
