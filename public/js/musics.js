21b234ec578a8ebd1979059dfe3a5cd33d168b7e
Omisakin Oluwatobi Samuel <oluwatob@ut.ee>
2016-12-20 19:58:00 +0200

    Remove logged id

--- public/js/musics.js
+++ public/js/musics.js
@@ -157,7 +157,6 @@ $(document).ready( function() {
     function playNext() {
         var mId = $("#fullList").find("tr.active").attr('id');
         var ID = mId.split("m").pop();
-        console.log(ID);
         document.title = "TP_PLayer";
         $.ajax({
             url: '/api/musics/'.concat(ID),
