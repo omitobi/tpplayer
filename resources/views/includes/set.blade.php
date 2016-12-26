d5824f13e832bb3c18e11ef60735a4c423aabb31
Omisakin Oluwatobi Samuel <oluwatob@ut.ee>
2016-12-26 15:37:17 +0200

    Remove repeated csrf

--- resources/views/includes/set.blade.php
+++ resources/views/includes/set.blade.php
@@ -2,7 +2,6 @@
     <form action="{{ route('music.add') }}" method="post">
         {{ csrf_field() }}
 <div class="col-xs-9   col-lg-8 col-lg-offset-2 col-xs-offset-0">
-    {{ csrf_field() }}
     <div class="form-group">
 
         <input type="text" name="link" class="form-control {{ $errors->has('link') ? 'has-error' : '' }}"
