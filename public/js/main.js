d57478be3cbf109f83f7d8fe0fa384a27d6563a5
Oluwatobi Samuel Omisakin <omisakin.oluwatobi@gmail.com>
2020-03-15 13:40:52 +0200

    Display alert once

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,14 @@
+
 $('document').ready(function () {
-   alert('Every Music links were only accessible because they were public.' +'' +
-       '\n The site is a developer\'s site only purpose to guide his learning process of proofing some concepts.' +
-       '\n If you trace any links that belongs to you here, you can send an email to "\omisakin.oluwatobi [at] gmail dat com\" to take it down.' +
-       '\nGenerally I love Gospel music and that\'s why I want to attempt making something that could edify me in more automated ways.' +
-       '\nBut in the coming weeks, The owner of the resources are being contacted to inform them of possible permission or actions' +
-       '\n' +
-       '\n Thank you.')
+   if (localStorage.getItem('warning_info') !== 'resolved') {
+      alert('Every Music links were only accessible because they were public.' + '' +
+          '\n The site is a developer\'s site only purpose to guide his learning process of proofing some concepts.' +
+          '\n If you trace any links that belongs to you here, you can send an email to "\omisakin.oluwatobi [at] gmail dat com\" to take it down.' +
+          '\nGenerally I love Gospel music and that\'s why I want to attempt making something that could edify me in more automated ways.' +
+          '\nBut in the coming weeks, The owner of the resources are being contacted to inform them of possible permission or actions' +
+          '\n' +
+          '\n Thank you.');
+
+      localStorage.setItem('warning_info', 'resolved');
+   }
 });
